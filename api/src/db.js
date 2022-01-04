c9a066fafe1245e4949a629f9d6791a51532c7e4
Carolina Espinoza <63523672+valen315@users.noreply.github.com>
2022-01-04 17:16:32 -0300

    carpeta api

---
+++ api/src/db.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const {
+  DB_USER, DB_PASSWORD, DB_HOST,
+} = process.env;
